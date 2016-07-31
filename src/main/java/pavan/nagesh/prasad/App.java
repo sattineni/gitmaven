cd6ea0156787b9435ecc496529241ec3849afcc0
prasad <prasad4308@gmail.com>
2016-07-31 12:20:08 +0530

    maven prasad

---
+++ src/main/java/pavan/nagesh/prasad/App.java
@@ -0,0 +1,13 @@
+package pavan.nagesh.prasad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
